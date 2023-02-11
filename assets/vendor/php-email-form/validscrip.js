c633b3d75f2e506f4e9d8cf77d75fb76a9beb27e
Akhil <Akhil@2000.com>
2023-02-11 13:09:28 +0530

    final

--- assets/vendor/php-email-form/validscrip.js
+++ assets/vendor/php-email-form/validscrip.js
@@ -1,45 +1,41 @@
-var Name=document.getElementById('name-error')
-var Email=document.getElementById('email-error')
+var Name = document.getElementById("name-error");
+var Email = document.getElementById("email-error");
 
+function validateName() {
+  var name = document.getElementById("name").value;
 
-
-function validateName(){
-  var name=document.getElementById('name').value;
-
-  if(name.length==0){
-    Name.innerHTML='Name is required';
+  if (name.length == 0) {
+    Name.innerHTML = "Name is required";
     return false;
   }
-  if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-    Name.innerHTML='full name is required';
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    Name.innerHTML = "full name is required";
     return false;
   }
-  Name.innerHTML='';
+  Name.innerHTML = "";
   return true;
 }
-function validateEmail(){
-  var email=document.getElementById('email').value;
+function validateEmail() {
+  var email = document.getElementById("email").value;
 
-  if(email.length==0){
-    Email.innerHTML='email is required';
+  if (email.length == 0) {
+    Email.innerHTML = "email is required";
     return false;
   }
-  if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-    Email.innerHTML='enter valid email';
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    Email.innerHTML = "enter valid email";
     return false;
   }
-  Email.innerHTML='';
+  Email.innerHTML = "";
   return true;
 }
 
-
-function validateForm(){
-  if(!validateName() || !validateEmail() ) {
-              submitError.style.display='block';
-              setTimeout(function() { submitError.style.display='none';},3000);
-              return false;
+function validateForm() {
+  if (!validateName() || !validateEmail()) {
+    submitError.style.display = "block";
+    setTimeout(function () {
+      submitError.style.display = "none";
+    }, 3000);
+    return false;
   }
 }
-
-
-
